dae387d47534728d778706e59d11a55fe6402b96
Bohdan Kibkalo <kibkalob@gmail.com>
2025-01-07 19:35:28 +0100

    Initial commit

---
+++ src/main/java/com/kibikalo/read_aware/ReadAwareApplication.java
@@ -0,0 +1,13 @@
+package com.kibikalo.read_aware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadAwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadAwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kibikalo/read_aware/ReadAwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibikalo.read_aware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadAwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
